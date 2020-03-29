5a45f475ee31add9a2a0a7e1d20e6ffb363f2bb5
PCdurham <47110600+PCdurham@users.noreply.github.com>
2020-03-29 08:26:54 +0100

    Update UAV2SEN_FuzzyCNN.py

--- code/UAV2SEN_FuzzyCNN.py
+++ code/UAV2SEN_FuzzyCNN.py
@@ -8,62 +8,148 @@ __license__ = 'MIT'
 
 ###############################################################################
 """ Libraries"""
-from keras import regularizers
-from keras import optimizers
+from tensorflow.keras import regularizers
+from tensorflow.keras import optimizers
+from tensorflow.keras.utils import normalize
 import numpy as np
 import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Flatten, Conv2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization,Flatten, Conv2D
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.utils import to_categorical
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
+from pickle import dump
+import seaborn as sns
+import statsmodels.api as sm
 
 
 
 #############################################################
-"""User data input. Fill in the info below before running"""
+"""User data input. Use the site template and list training and validation choices"""
 #############################################################
-
-# Edit this section to have as many inputs as the number of times you ran UAV2SEN_MakeFuzzyTensor
-Train1 = np.load('F:\\MixClass\\SesiaCarr_T.npy')
-Label1 = np.load('F:\\MixClass\\SesiaCarr_L.npy')
-
-Train2 = np.load('F:\\MixClass\\BuonAmico_T.npy')
-Label2 = np.load('F:\\MixClass\\BuonAmico_L.npy')
-
-Train3 = np.load('F:\\MixClass\\BuonAmicoValle_T.npy')
-Label3 = np.load('F:\\MixClass\\BuonAmicoVale_L.npy')
-
-Train4 = np.load('F:\\MixClass\\Po_T.npy')
-Label4 = np.load('F:\\MixClass\\Po_L.npy')
+MainData = 'E:\\UAV2SEN\\MLdata\\CNNDebug'  #main data output from UAV2SEN_MakeCrispTensor.py. no extensions, will be fleshed out below
+SiteList = 'E:\\UAV2SEN\\SiteList.csv'#this has the lists of sites with name, month, year and 1s and 0s to identify training and validation sites
+ModelName = 'E:\\UAV2SEN\\MLdata\\CNNdebugged.h5'  #Name and location of the final model to be saved
+TrainingEpochs = 100 #Typically this can be reduced
+Nfilters = 32
+size=5#size of the tensor tiles
+KernelSize=5 # size of the convolution kernels
+UT=0.95# upper and lower thresholds to elimn=inate pure classes from fuzzy error estimates
+LT=0.05
 
 
-TrainData = np.concatenate([Train1,Train2, Train3, Train4], axis=0)
-TrainLabel = np.concatenate([Label1,Label2, Label3, Label4], axis=0)
+FeatureSet =  ['B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
+LabelSet = ['WaterMem', 'VegMem','SedMem' ]
+LearningRate = 0.001
+Chatty = 1 # set the verbosity of the model training. 
+NAF = 'relu' #NN activation function
+
+DoHistory = False #Plot the history of the training losses
+
+'''Load the tensors and filter out the required training and validation data.'''
+TensorFileName = MainData+'_fuzzy_'+str(size)+'_T.npy'
+LabelFileName = MainData+'_fuzzy_'+str(size)+'_L.dat'
+
+SiteDF = pd.read_csv(SiteList)
+MasterTensor = np.load(TensorFileName)
+MasterLabelDF=pd.read_feather(LabelFileName)
+
+#Select the features in the tensor
+
+Valid=np.zeros(12)
+for n in range(1,13):
+    if ('B'+str(n)) in FeatureSet:
+        Valid[n-1]=1
+        
+MasterTensor = np.compress(Valid, MasterTensor, axis=3)
 
 
 
-'''BASIC PARAMETER CHOICES'''
 
-TrainingEpochs = 150 #For NN training
-Nfilters=36
-TTS = 0.0001 #Train test split fraction in test.  Keep to 10E-4 if not using
-NClasses = 3  #The number of end members in the fuzzy class
-inShape = (5,5,11) #adjust this to the template size in X, Y and number of bands
-'''MODEL PARAMETERS''' #These would usually not be edited
+#Start the filter process to isolate training and validation data
+TrainingSites = SiteDF[SiteDF.Training == 1]
+ValidationSites = SiteDF[SiteDF.Validation == 1]
+
+#isolate the site. first labels then tensors
+TrainDF = MasterLabelDF[MasterLabelDF['Site'].isin(TrainingSites.Abbrev.to_list())]
+ValidationDF = MasterLabelDF[MasterLabelDF['Site'].isin(ValidationSites.Abbrev.to_list())]
+
+Valid = MasterLabelDF['Site'].isin(TrainingSites.Abbrev.to_list())
+TrainingTensor = np.compress(Valid, MasterTensor, axis=0)
+Valid = MasterLabelDF['Site'].isin(ValidationSites.Abbrev.to_list())
+ValidationTensor = np.compress(Valid, MasterTensor, axis=0)
+
+#isolate the year
+TrainDF = TrainDF[TrainDF['Year'].isin(TrainingSites.Year.to_list())]
+ValidationDF = ValidationDF[ValidationDF['Year'].isin(ValidationSites.Year.to_list())]
+
+Valid= TrainDF['Year'].isin(TrainingSites.Year.to_list())
+TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
+Valid = ValidationDF['Year'].isin(ValidationSites.Year.to_list())
+ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
+
+
+#isolate the month
+TrainDF = TrainDF[TrainDF['Month'].isin(TrainingSites.Month.to_list())]
+ValidationDF = ValidationDF[ValidationDF['Month'].isin(ValidationSites.Month.to_list())]
+
+Valid= TrainDF['Month'].isin(TrainingSites.Month.to_list())
+TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
+Valid = ValidationDF['Month'].isin(ValidationSites.Month.to_list())
+ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
+
+#Set the labels
+TrainLabels = TrainDF[LabelSet]
+ValidationLabels = ValidationDF[LabelSet]
+    
+#check for empty dataframes and raise an error if found
+
+if (len(TrainDF.index)==0):
+    raise Exception('There is an empty dataframe for TRAINING')
+    
+if (len(ValidationDF.index)==0):
+    raise Exception('There is an empty dataframe for VALIDATION')
+    
+#Check that tensor lengths match label lengths
+
+if (len(TrainLabels.index)) != TrainingTensor.shape[0]:
+    raise Exception('Sample number mismatch for TRAINING tensor and labels')
+    
+if (len(ValidationLabels.index)) != ValidationTensor.shape[0]:
+    raise Exception('Sample number mismatch for VALIDATION tensor and labels')
+    
+
+
+        
+    
+'''Range the training tensor from 0-1'''
+#NormFactor = np.max(np.unique(TrainingTensor.reshape(1,-1)))
+#TrainingTensor = TrainingTensor/NormFactor
+#ValidationTensor = ValidationTensor/NormFactor
+#TrainingTensor = normalize(TrainingTensor)
+#ValidationTensor = normalize(ValidationTensor)
  
-Ndims = TrainData.shape[3] # Feature Dimensions. 4 if using entropy in phase 2, 3 if just RGB
-LearningRate = 0.0001
-Chatty = 1 # set the verbosity of the model training.  Use 1 at first, 0 when confident that model is well tuned
-NAF = 'relu' #Neural network Activation Function
+Ndims = TrainingTensor.shape[3] # Feature Dimensions. 
+NClasses = 3  #The number of classes in the data. This MUST be the same as the classes used to retrain the model
+inShape = TrainingTensor.shape[1:]
 
-####################################################################################################################
+
+
 
 ##############################################################################
-"""Instantiate the CNN regressor""" 
+"""Instantiate the Neural Network pixel-based classifier""" 
+#EstimatorRF = RFC(n_estimators = 150, n_jobs = 8, verbose = Chatty) #adjust this to your processors
    
 
+
+# define the very deep model with L2 regularization and dropout
+
+ 	# create model
 Estimator = Sequential()
-Estimator.add(Conv2D(Nfilters,5, data_format='channels_last', input_shape=inShape, activation=NAF))
+Estimator.add(Conv2D(Nfilters,KernelSize, data_format='channels_last', input_shape=inShape, activation=NAF))
+#Estimator.add(Conv2D(Nfilters,KernelSize, activation=NAF))
 Estimator.add(Flatten())
 Estimator.add(Dense(64, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
 Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
@@ -76,24 +162,140 @@ Optim = optimizers.Adam(lr=LearningRate, beta_1=0.9, beta_2=0.999, decay=0.0, am
 
 # Compile model
 Estimator.compile(loss='mean_squared_error', optimizer=Optim, metrics = ['accuracy'])
-#Estimator.summary()
-
-
-
-
+Estimator.summary()
+  
 ###############################################################################
-"""Data"""
+"""Data Fitting"""
+
+X_train, X_test, y_train, y_test = train_test_split(TrainingTensor, TrainLabels, test_size=0.2, random_state=42)
+print('Fitting CNN Classifier on ' + str(len(X_train)) + ' pixels')
+Estimator.fit(X_train, y_train, batch_size=1000, epochs=TrainingEpochs, verbose=Chatty)#, class_weight=weights)
+#EstimatorRF.fit(X_train, y_train)
+    
 
 
-X = TrainData
-Y = TrainLabel
-
-#weights = class_weight.compute_class_weight('balanced', np.unique(Y),  Y)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=TTS, random_state=101)
+'''Save model'''
+Estimator.save(ModelName,save_format='h5')
 
 
-print('Fitting CNN fuzzy regressor on ' + str(len(X_train)) + ' pixels')
-Estimator.fit(X_train, y_train, batch_size=1000, epochs=TrainingEpochs, verbose=Chatty)
+'''Validate the model'''
+#Test data
+PredictedPixels = Estimator.predict(X_test)
+Y=y_test
+
+Error1 = PredictedPixels[:,0] - Y.WaterMem
+Error2 = PredictedPixels[:,1] - Y.VegMem
+Error3 = PredictedPixels[:,2] - Y.SedMem
 
 
-print('Fuzzy CNN model trained, use other scripts to test validation data')
+ErrFrame = pd.DataFrame({'C1 Err':Error1, 'C2 Err':Error2, 'C3 Err':Error3, 'C1 Obs':Y.WaterMem, 'C2 Obs':Y.VegMem,'C3 Obs':Y.SedMem, 'C1 Pred':PredictedPixels[:,0], 'C2 Pred':PredictedPixels[:,1],'C3 Pred':PredictedPixels[:,2]})
+ErrFrame1 = ErrFrame[ErrFrame['C1 Obs']<UT]
+ErrFrame1 = ErrFrame1[ErrFrame1['C1 Obs']>LT]
+jplot = sns.jointplot("C1 Obs", 'C1 Pred', data=ErrFrame1, kind="kde", color='b', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+ErrFrame2 = ErrFrame[ErrFrame['C2 Obs']<UT]
+ErrFrame2 = ErrFrame2[ErrFrame2['C2 Obs']>LT]
+jplot = sns.jointplot("C2 Obs", 'C2 Pred', data=ErrFrame2, kind="kde", color='g', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+ErrFrame3 = ErrFrame[ErrFrame['C3 Obs']<UT]
+ErrFrame3 = ErrFrame3[ErrFrame3['C3 Obs']>LT]
+jplot = sns.jointplot("C3 Obs", 'C3 Pred', data=ErrFrame3, kind="kde", color='r', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+Error1 = ErrFrame1['C1 Err']
+Error2 = ErrFrame2['C2 Err']
+Error3 = ErrFrame3['C3 Err']
+RMS1 = np.sqrt(np.mean(Error1*Error1))
+RMS2 = np.sqrt(np.mean(Error2*Error2))
+RMS3 = np.sqrt(np.mean(Error3*Error3))
+Errors = np.concatenate((Error1, Error2, Error3))
+RMSall = np.sqrt(np.mean(Errors*Errors))
+print('20% test mean error =', str(np.mean(Errors)))
+print('20% test RMS error =', str(RMSall))
+print('\n')
+print('Water Fit Stats')
+X=sm.add_constant(ErrFrame1['C1 Obs'])
+linemodel = sm.OLS(ErrFrame1['C1 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())
+
+print('Veg Fit Stats')
+X=sm.add_constant(ErrFrame2['C2 Obs'])
+linemodel = sm.OLS(ErrFrame2['C2 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())
+
+print('Sed Fit Stats')
+X=sm.add_constant(ErrFrame3['C3 Obs'])
+linemodel = sm.OLS(ErrFrame3['C3 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())#
+
+
+#Validation data
+PredictedPixels = Estimator.predict(ValidationTensor)
+Y=ValidationLabels
+
+Error1 = PredictedPixels[:,0] - Y.WaterMem
+Error2 = PredictedPixels[:,1] - Y.VegMem
+Error3 = PredictedPixels[:,2] - Y.SedMem
+
+
+ErrFrame = pd.DataFrame({'C1 Err':Error1, 'C2 Err':Error2, 'C3 Err':Error3, 'C1 Obs':Y.WaterMem, 'C2 Obs':Y.VegMem,'C3 Obs':Y.SedMem, 'C1 Pred':PredictedPixels[:,0], 'C2 Pred':PredictedPixels[:,1],'C3 Pred':PredictedPixels[:,2]})
+ErrFrame1 = ErrFrame[ErrFrame['C1 Obs']<UT]
+ErrFrame1 = ErrFrame1[ErrFrame1['C1 Obs']>LT]
+jplot = sns.jointplot("C1 Obs", 'C1 Pred', data=ErrFrame1, kind="kde", color='b', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+ErrFrame2 = ErrFrame[ErrFrame['C2 Obs']<UT]
+ErrFrame2 = ErrFrame2[ErrFrame2['C2 Obs']>LT]
+jplot = sns.jointplot("C2 Obs", 'C2 Pred', data=ErrFrame2, kind="kde", color='g', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+ErrFrame3 = ErrFrame[ErrFrame['C3 Obs']<UT]
+ErrFrame3 = ErrFrame3[ErrFrame3['C3 Obs']>LT]
+jplot = sns.jointplot("C3 Obs", 'C3 Pred', data=ErrFrame3, kind="kde", color='r', n_levels=500)
+jplot.ax_marg_x.set_xlim(-0.2, 1.2)
+jplot.ax_marg_y.set_ylim(-0.2, 1.2)
+
+
+Error1 = ErrFrame1['C1 Err']
+Error2 = ErrFrame2['C2 Err']
+Error3 = ErrFrame3['C3 Err']
+RMS1 = np.sqrt(np.mean(Error1*Error1))
+RMS2 = np.sqrt(np.mean(Error2*Error2))
+RMS3 = np.sqrt(np.mean(Error3*Error3))
+Errors = np.concatenate((Error1, Error2, Error3))
+RMSall = np.sqrt(np.mean(Errors*Errors))
+print('Validation mean error =', str(np.mean(Errors)))
+print('Validation RMS error =', str(RMSall))
+print('\n')
+print('Water Fit Stats')
+X=sm.add_constant(ErrFrame1['C1 Obs'])
+linemodel = sm.OLS(ErrFrame1['C1 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())
+
+print('Veg Fit Stats')
+X=sm.add_constant(ErrFrame2['C2 Obs'])
+linemodel = sm.OLS(ErrFrame2['C2 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())
+
+print('Sed Fit Stats')
+X=sm.add_constant(ErrFrame3['C3 Obs'])
+linemodel = sm.OLS(ErrFrame3['C3 Pred'], X)
+reg = linemodel.fit()
+print(reg.summary())#
