d90a5425907f9909e94d524dde95bec0ebe34809
PCdurham <47110600+PCdurham@users.noreply.github.com>
2020-03-31 14:49:28 +0100

    Update UAV2SEN_CrispCNN2.py

--- code/UAV2SEN_CrispCNN2.py
+++ code/UAV2SEN_CrispCNN2.py
@@ -21,7 +21,10 @@ from tensorflow.keras.utils import to_categorical
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from pickle import dump
+from skimage import io
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.patches as mpatches
 
 
 
@@ -32,21 +35,43 @@ from pickle import dump
 MainData = 'E:\\UAV2SEN\\MLdata\\FullData_4xnoise'  #main data output from UAV2SEN_MakeCrispTensor.py. no extensions, will be fleshed out below
 SiteList = 'E:\\UAV2SEN\\SiteList.csv'#this has the lists of sites with name, month, year and 1s and 0s to identify training and validation sites
 DatFolder = 'E:\\UAV2SEN\\FinalTif\\'  #location of above
-TrainingEpochs = 50 #Typically this can be reduced
-Nfilters = 128
+TrainingEpochs = 20 #Typically this can be reduced
+Nfilters = 64
 UAVtrain = True #if true use the UAV class data to train the model, if false use desk-based
 UAVvalid = True #if true use the UAV class data to validate.  If false, use desk-based polygons
 size=5#size of the tensor tiles
-KernelSize=5 # size of the convolution kernels
-MajType= 'Pure' #Majority type. only used if UAVtrain or valid is true. The options are RelMaj (relative majority), Maj (majority) and Pure (95% unanimous).
-
-FeatureSet = ['B8','B9','B10','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
+KernelSize=3 # size of the convolution kernels
+MajType= 'Pure' #Majority type. only used if UAVtrain or valid is true. The options are RelMaj (relative majority class), Maj (majority) and Pure (95% unanimous).
+ShowValidation = True #if true will show predicted class rasters for validation images from the site list
+FeatureSet = ['B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
 
 LearningRate = 0.001
 Chatty = 1 # set the verbosity of the model training. 
 NAF = 'relu' #NN activation function
 
 DoHistory = False #Plot the history of the training losses
+
+
+#################################################################################
+'''Function definitions'''
+def slide_raster_to_tiles(im, size):
+    h=im.shape[0]
+    w=im.shape[1]
+    di=im.shape[2]
+    TileTensor = np.zeros(((h-size)*(w-size), size,size,di))
+
+    
+    B=0
+    for y in range(0, h-size):
+        for x in range(0, w-size):
+
+            TileTensor[B,:,:,:] = im[y:y+size,x:x+size,:]
+            B+=1
+
+    return TileTensor
+
+
+
 
 '''Load the tensors and filter out the required training and validation data.'''
 TensorFileName = MainData+'_crisp_'+str(size)+'_T.npy'
@@ -86,7 +111,7 @@ for s in range(len(TrainingSites.Site)):
 TrainingDF = TrainingDF.loc[MasterValid]
 TrainingTensor=np.compress(MasterValid,TrainingTensor, axis=0)#will delete where valid is false
 
-MasterValid = (np.zeros(len(MasterLabelDF.index)))
+MasterValid = (np.zeros(len(MasterLabelDF.index)))==1
 for s in range(len(ValidationSites.Site)):
     Valid = (ValidationDF.Site == ValidationSites.Abbrev[s])&(ValidationDF.Year==ValidationSites.Year[s])&(ValidationDF.Month==ValidationSites.Month[s])
     MasterValid = MasterValid | Valid
@@ -95,107 +120,45 @@ ValidationDF = ValidationDF.loc[MasterValid]
 ValidationTensor = np.compress(MasterValid,ValidationTensor, axis=0)#will delete where valid is false 
 
     
-
+MajType=MajType+'Class'
 
 
 #select desk-based or UAV-based for training and validation, if using UAV data, select the majority type
 if UAVtrain & UAVvalid:
-    TrainingDF=TrainingDF[TrainingDF.PolyClass==-1]
-    ValidationDF=ValidationDF[ValidationDF.PolyClass==-1]
-    Valid= TrainingDF.PolyClass==-1
-    TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-    Valid = ValidationDF.PolyClass==-1
-    ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    if 'RelMaj' in  MajType:
-        TrainingDF = TrainingDF[TrainingDF.RelMajClass>0]
-        ValidationDF = ValidationDF[ValidationDF.RelMajClass>0]
-        TrainLabels = TrainingDF.RelMajClass
-        ValidationLabels = ValidationDF.RelMajClass
-        Valid= TrainingDF.RelMajClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-        Valid = ValidationDF.RelMajClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    elif 'Maj' in MajType:
-        TrainingDF = TrainingDF[TrainingDF.MajClass>0]
-        ValidationDF = ValidationDF[ValidationDF.MajClass>0]
-        TrainLabels = TrainingDF.MajClass
-        ValidationLabels = ValidationDF.MajClass
-        Valid= TrainingDF.MajClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-        Valid = ValidationDF.MajClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
+    TrainLabels= TrainingDF[MajType]
+    ValidationLabels = ValidationDF[MajType]
+    TrainingTensor = np.compress(TrainLabels>0, TrainingTensor, axis=0)
+    ValidationTensor = np.compress(ValidationLabels>0,ValidationTensor, axis=0)
+    TrainLabels=TrainLabels.loc[TrainLabels>0]
+    ValidationLabels=ValidationLabels.loc[ValidationLabels>0]
     
-    elif 'Pure' in MajType:
-        TrainingDF = TrainingDF[TrainingDF.PureClass>0]
-        ValidationDF = ValidationDF[ValidationDF.PureClass>0]
-        TrainLabels = TrainingDF.PureClass
-        ValidationLabels = ValidationDF.PureClass
-        Valid= TrainingDF.PureClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-        Valid = ValidationDF.PureClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-        
+   
 elif UAVtrain and ~(UAVvalid):
-    TrainingDF=TrainingDF[TrainingDF.PolyClass==-1]
-    ValidationDF=ValidationDF[ValidationDF.PolyClass>0]
+    TrainLabels= TrainingDF[MajType]
     ValidationLabels = ValidationDF.PolyClass
-    Valid= TrainingDF.PolyClass==-1
-    TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-    Valid = ValidationDF.PolyClass>0
-    ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    if 'RelMaj' in  MajType:
-        TrainingDF = TrainingDF[TrainingDF.RelMajClass>0]
-        TrainLabels = TrainingDF.RelMajClass
-        Valid= TrainingDF.RelMajClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-
-    elif 'Maj' in MajType:
-        TrainingDF = TrainingDF[TrainingDF.MajClass>0]
-        TrainLabels = TrainingDF.MajClass
-        Valid= TrainingDF.MajClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
+    TrainingTensor = np.compress(TrainLabels>0, TrainingTensor, axis=0)
+    ValidationTensor = np.compress(ValidationLabels>0,ValidationTensor, axis=0)
+    TrainLabels=TrainLabels.loc[TrainLabels>0]
+    ValidationLabels=ValidationLabels.loc[ValidationLabels>0]
     
-    elif 'Pure' in MajType:
-        TrainingDF = TrainingDF[TrainingDF.PureClass>0]
-        TrainLabels = TrainingDF.PureClass
-        Valid= TrainingDF.PureClass>0
-        TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-        
 elif ~(UAVtrain) & UAVvalid:
-    TrainingDF=TrainingDF[TrainingDF.PolyClass>0]
-    TrainLabels = TrainingDF.PolyClass
-    ValidationDF=ValidationDF[ValidationDF.PolyClass==-1]
-    Valid= TrainingDF.PolyClass>0
-    TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-    Valid = ValidationDF.PolyClass==-1
-    ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    if 'RelMaj' in  MajType:
-        ValidationDF = ValidationDF[ValidationDF.RelMajClass>0]
-        ValidationLabels = ValidationDF.RelMajClass
-        Valid = ValidationDF.RelMajClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    elif 'Maj' in MajType:
-        ValidationDF = ValidationDF[ValidationDF.MajClass>0]
-        ValidationLabels = ValidationDF.MajClass
-        Valid = ValidationDF.MajClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
+    TrainLabels= TrainingDF.PolyClass
+    ValidationLabels = ValidationDF[MajType]
+    TrainingTensor = np.compress(TrainLabels>0, TrainingTensor, axis=0)
+    ValidationTensor = np.compress(ValidationLabels>0,ValidationTensor, axis=0)
+    TrainLabels=TrainLabels.loc[TrainLabels>0]
+    ValidationLabels=ValidationLabels.loc[ValidationLabels>0]
     
-    elif 'Pure' in MajType:
-        ValidationDF = ValidationDF[ValidationDF.PureClass>0]
-        ValidationLabels = ValidationDF.PureClass
-        Valid = ValidationDF.ValidationDF.PureClass>0
-        ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
-    
+#
 else:
-    TrainingDF=TrainingDF[TrainingDF.PolyClass>0]
-    ValidationDF=ValidationDF[ValidationDF.PolyClass>0]
-    TrainLabels = TrainingDF.PolyClass
-    ValidationDF=ValidationDF[ValidationDF.PolyClass>0]
+    TrainLabels= TrainingDF.PolyClass
     ValidationLabels = ValidationDF.PolyClass
-    Valid= TrainingDF.PolyClass>0
-    TrainingTensor = np.compress(Valid, TrainingTensor, axis=0)
-    Valid = ValidationDF.PolyClass>0
-    ValidationTensor = np.compress(Valid, ValidationTensor, axis=0)
+    TrainingTensor = np.compress(TrainLabels>0, TrainingTensor, axis=0)
+    ValidationTensor = np.compress(ValidationLabels>0,ValidationTensor, axis=0)
+    TrainLabels=TrainLabels.loc[TrainLabels>0]
+    ValidationLabels=ValidationLabels.loc[ValidationLabels>0]
+    
+#   
     
 #check for empty dataframes and raise an error if found
 
@@ -240,15 +203,41 @@ inShape = TrainingTensor.shape[1:]
 # define the very deep model with L2 regularization and dropout
 
  	# create model
-Estimator = Sequential()
-Estimator.add(Conv2D(Nfilters,5, data_format='channels_last', input_shape=(5,5,Ndims), activation=NAF))
-Estimator.add(Flatten())
-Estimator.add(Dense(128, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
-Estimator.add(Dense(64, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='softmax'))
+if size==3: 
+    Estimator = Sequential()
+    Estimator.add(Conv2D(Nfilters,KernelSize, data_format='channels_last', input_shape=inShape, activation=NAF))
+    Estimator.add(Flatten())
+    Estimator.add(Dense(64, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+    Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='softmax'))    
 
+
+elif size==5:
+    Estimator = Sequential()
+    Estimator.add(Conv2D(Nfilters,KernelSize, data_format='channels_last', input_shape=inShape, activation=NAF))
+    Estimator.add(Conv2D(Nfilters//2,KernelSize, activation=NAF))
+    Estimator.add(Flatten())
+    Estimator.add(Dense(64, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+    Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='softmax'))
+    
+elif size==7:
+    Estimator = Sequential()
+    Estimator.add(Conv2D(Nfilters,KernelSize, data_format='channels_last', input_shape=inShape, activation=NAF))
+    Estimator.add(Conv2D(Nfilters//2,KernelSize, activation=NAF))
+    Estimator.add(Conv2D(Nfilters//4,KernelSize, activation=NAF))
+    Estimator.add(Flatten())
+    Estimator.add(Dense(64, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
+    Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
+    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='softmax'))
+else:
+    raise Exception('Invalid tile size, only 3,5 and 7 available')
 #Tune an optimiser
 Optim = optimizers.Adam(lr=LearningRate, beta_1=0.9, beta_2=0.999, decay=0.0, amsgrad=True)
 
@@ -283,4 +272,58 @@ print('Out-of-Sample validation results for ')
 print(report)
 
 
-'''Save model and scaler'''
+'''Show the classified validation images'''
+if ShowValidation:
+    for s in range(len(ValidationSites.index)):
+        UAVRaster = io.imread(DatFolder+ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s])+'_UAVCLS.tif')
+        UAVRaster[UAVRaster>3] =0
+        UAVRaster[UAVRaster<1] =0
+        UAVRasterRGB = np.zeros((UAVRaster.shape[0], UAVRaster.shape[1], 3))
+        UAVRasterRGB[:,:,0]=255*(UAVRaster==3)
+        UAVRasterRGB[:,:,1]=255*(UAVRaster==2)
+        UAVRasterRGB[:,:,2]=255*(UAVRaster==1)
+        
+        ValidRasterName = DatFolder+ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s])+'_S2.tif'
+        ValidRaster = io.imread(ValidRasterName)
+        ValidRasterIR = np.zeros((ValidRaster.shape[0], ValidRaster.shape[1],3))
+        ValidRasterIR[:,:,0] = ValidRaster[:,:,10]
+        ValidRasterIR[:,:,1] = ValidRaster[:,:,4]
+        ValidRasterIR[:,:,2] = ValidRaster[:,:,3]
+        ValidRasterIR = ValidRasterIR/np.max(np.unique(ValidRasterIR))
+        ValidTensor = slide_raster_to_tiles(ValidRaster, size)
+        Valid=np.zeros(12)
+        for n in range(1,13):
+            if ('B'+str(n)) in FeatureSet:
+                Valid[n-1]=1
+        
+        ValidTensor = np.compress(Valid, ValidTensor, axis=3)
+        #print(ValidTensor.shape)
+        PredictedPixels = Estimator.predict(ValidTensor)
+        PredictedPixels =np.argmax(PredictedPixels, axis=1)
+        
+
+        PredictedPixels = np.int16(PredictedPixels.reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
+        PredictedClassImage=np.int16(np.zeros((PredictedPixels.shape[0], PredictedPixels.shape[1],3)))
+        PredictedClassImage[:,:,0]=255*(PredictedPixels==3)
+        PredictedClassImage[:,:,1]=255*(PredictedPixels==2)
+        PredictedClassImage[:,:,2]=255*(PredictedPixels==1)
+
+        cmapCHM = colors.ListedColormap(['black','red','lime','blue'])
+        plt.figure()
+        plt.subplot(2,2,1)
+        plt.imshow(np.int16(255*(ValidRasterIR)))
+        plt.title(ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s]) + ' Bands (11,3,2)')
+        plt.subplot(2,2,2)
+        plt.imshow(PredictedClassImage)
+        plt.title(ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s]) + ' Crisp Class')
+        class1_box = mpatches.Patch(color='red', label='Sediment')
+        class2_box = mpatches.Patch(color='lime', label='Veg.')
+        class3_box = mpatches.Patch(color='blue', label='Water')
+        ax=plt.gca()
+        ax.legend(handles=[class1_box,class2_box,class3_box], bbox_to_anchor=(1, -0.2),prop={'size': 30})
+        
+        #ax.legend(handles=[class1_box,class2_box,class3_box])
+        plt.subplot(2,2,3)
+        plt.imshow(np.int16(UAVRasterRGB))
+        plt.xlabel('UAV Ground-Truth Data')
+        
