3318337e9ac22dcd55207ded1c5ca9a9d98693b1
PCdurham <47110600+PCdurham@users.noreply.github.com>
2020-03-31 12:23:35 +0100

    Update UAV2SEN_FuzzyCNN.py

--- code/UAV2SEN_FuzzyCNN.py
+++ code/UAV2SEN_FuzzyCNN.py
@@ -33,19 +33,19 @@ import matplotlib.patches as mpatches
 #############################################################
 """User data input. Use the site template and list training and validation choices"""
 #############################################################
-MainData = 'E:\\UAV2SEN\\MLdata\\CNNDebug'  #main data output from UAV2SEN_MakeCrispTensor.py. no extensions, will be fleshed out below
+MainData = 'E:\\UAV2SEN\\MLdata\\FullData_4xnoise'  #main data output from UAV2SEN_MakeCrispTensor.py. no extensions, will be fleshed out below
 SiteList = 'E:\\UAV2SEN\\SiteList.csv'#this has the lists of sites with name, month, year and 1s and 0s to identify training and validation sites
 ModelName = 'E:\\UAV2SEN\\MLdata\\CNNdebugged.h5'  #Name and location of the final model to be saved
 DatFolder = 'E:\\UAV2SEN\\FinalTif\\'  #location of processed tif files
-TrainingEpochs = 15 #Typically this can be reduced
+TrainingEpochs = 100 #Typically this can be reduced
 Nfilters = 64 #powers of 2 only
 size=5#size of the tensor tiles
 KernelSize=3 # size of the convolution kernels. Caution becasue mis-setting this could cause bugs in the network definition.  Best keep at 3.
 UT=1.95# upper and lower thresholds to elimninate pure classes from fuzzy error estimates if needed
 LT=-0.05
-ShowValidation = True
+ShowValidation = True#if true fuzzy classified images of the validation sites will be displayed
 
-FeatureSet =  ['B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
+FeatureSet =  ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
 LabelSet = ['WaterMem', 'VegMem','SedMem' ]
 LearningRate = 0.001
 Chatty = 1 # set the verbosity of the model training. 
@@ -78,11 +78,11 @@ def slide_raster_to_tiles(im, size):
 
 '''Load the tensors and filter out the required training and validation data.'''
 TensorFileName = MainData+'_fuzzy_'+str(size)+'_T.npy'
-LabelFileName = MainData+'_fuzzy_'+str(size)+'_L.dat'
+LabelFileName = MainData+'_fuzzy_'+str(size)+'_L.csv'
 
 SiteDF = pd.read_csv(SiteList)
 MasterTensor = np.load(TensorFileName)
-MasterLabelDF=pd.read_feather(LabelFileName)
+MasterLabelDF=pd.read_csv(LabelFileName)
 
 #Select the features in the tensor
 
@@ -176,7 +176,7 @@ if size==3:
     Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
     Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
     Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='linear'))    
+    Estimator.add(Dense(NClasses, kernel_initializer='normal', activation='linear'))    
 
 
 elif size==5:
@@ -188,7 +188,7 @@ elif size==5:
     Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
     Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
     Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='linear'))
+    Estimator.add(Dense(NClasses, kernel_initializer='normal', activation='linear'))
     
 elif size==7:
     Estimator = Sequential()
@@ -200,7 +200,7 @@ elif size==7:
     Estimator.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True, beta_initializer='zeros', gamma_initializer='ones', moving_mean_initializer='zeros', moving_variance_initializer='ones', beta_regularizer=None, gamma_regularizer=None, beta_constraint=None, gamma_constraint=None))
     Estimator.add(Dense(32, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
     Estimator.add(Dense(16, kernel_regularizer= regularizers.l2(0.001), kernel_initializer='normal', activation=NAF))
-    Estimator.add(Dense(NClasses+1, kernel_initializer='normal', activation='linear'))
+    Estimator.add(Dense(NClasses, kernel_initializer='normal', activation='linear'))
 else:
     raise Exception('Invalid tile size, only 3,5 and 7 available')
 
@@ -351,8 +351,21 @@ print(reg.summary())#
 '''Show the classified validation images'''
 if ShowValidation:
     for s in range(len(ValidationSites.index)):
+        UAVRaster = io.imread(DatFolder+ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s])+'_UAVCLS.tif')
+        UAVRaster[UAVRaster>3] =0
+        UAVRaster[UAVRaster<1] =0
+        UAVRasterRGB = np.zeros((UAVRaster.shape[0], UAVRaster.shape[1], 3))
+        UAVRasterRGB[:,:,0]=255*(UAVRaster==3)
+        UAVRasterRGB[:,:,1]=255*(UAVRaster==2)
+        UAVRasterRGB[:,:,2]=255*(UAVRaster==1)
+        
         ValidRasterName = DatFolder+ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s])+'_S2.tif'
         ValidRaster = io.imread(ValidRasterName)
+        ValidRasterIR = np.zeros((ValidRaster.shape[0], ValidRaster.shape[1],3))
+        ValidRasterIR[:,:,0] = ValidRaster[:,:,10]
+        ValidRasterIR[:,:,1] = ValidRaster[:,:,4]
+        ValidRasterIR[:,:,2] = ValidRaster[:,:,3]
+        ValidRasterIR = ValidRasterIR/np.max(np.unique(ValidRasterIR))
         ValidTensor = slide_raster_to_tiles(ValidRaster, size)
         Valid=np.zeros(12)
         for n in range(1,13):
@@ -360,24 +373,39 @@ if ShowValidation:
                 Valid[n-1]=1
         
         ValidTensor = np.compress(Valid, ValidTensor, axis=3)
-        print(ValidTensor.shape)
+        #print(ValidTensor.shape)
         PredictedPixels = Estimator.predict(ValidTensor)
         
 
-        PredictedWaterImage = np.uint8(255*PredictedPixels[:,0].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
-        PredictedVegImage = np.uint8(255*PredictedPixels[:,1].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
-        PredictedSedImage = np.uint8(255*PredictedPixels[:,2].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
-        PredictedClassImage=np.concat((PredictedSedImage,PredictedVegImage,PredictedWaterImage), axis=2)
+        PredictedWaterImage = np.int16(255*PredictedPixels[:,0].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
+        PredictedVegImage = np.int16(255*PredictedPixels[:,1].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
+        PredictedSedImage = np.int16(255*PredictedPixels[:,2].reshape(ValidRaster.shape[0]-size, ValidRaster.shape[1]-size))
+        PredictedClassImage=np.int16(np.zeros((PredictedWaterImage.shape[0], PredictedWaterImage.shape[1],3)))
+        PredictedClassImage[:,:,0]=PredictedSedImage
+        PredictedClassImage[:,:,1]=PredictedVegImage
+        PredictedClassImage[:,:,2]=PredictedWaterImage
 
-        cmapCHM = colors.ListedColormap(['black','red','green','blue'])
+        cmapCHM = colors.ListedColormap(['black','red','lime','blue'])
         plt.figure()
+        plt.subplot(2,2,1)
+        plt.imshow(np.int16(255*(ValidRasterIR)))
+        plt.title(ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s]) + ' Bands (11,3,2)')
+        plt.subplot(2,2,2)
         plt.imshow(PredictedClassImage)
-        
+        plt.title(ValidationSites.Abbrev[s]+'_'+str(ValidationSites.Month[s])+'_'+str(ValidationSites.Year[s]) + ' Fuzzy Class')
         class1_box = mpatches.Patch(color='red', label='Sediment')
-        class2_box = mpatches.Patch(color='green', label='Veg.')
+        class2_box = mpatches.Patch(color='lime', label='Veg.')
         class3_box = mpatches.Patch(color='blue', label='Water')
         ax=plt.gca()
-        ax.legend(handles=[class1_box,class2_box,class3_box])
-        plt.title(ValidRasterName)
+        ax.legend(handles=[class1_box,class2_box,class3_box], bbox_to_anchor=(1, -0.2),prop={'size': 30})
+        
+        #ax.legend(handles=[class1_box,class2_box,class3_box])
+        plt.subplot(2,2,3)
+        plt.imshow(np.int16(UAVRasterRGB))
+        plt.xlabel('UAV Ground-Truth Data')
+        
+        
+        
+    
 
 
