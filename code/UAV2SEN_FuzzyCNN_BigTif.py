f912bdd53486e4a585395882a426f5a03539c25d
PCdurham <47110600+PCdurham@users.noreply.github.com>
2020-04-01 15:41:13 +0100

    Update UAV2SEN_FuzzyCNN_BigTif.py

--- code/UAV2SEN_FuzzyCNN_BigTif.py
+++ code/UAV2SEN_FuzzyCNN_BigTif.py
@@ -38,8 +38,8 @@ size=5 #tile sized used to train the model
 FeatureSet =  ['B2','B3','B4','B5','B6','B7','B8','B9','B11','B12'] # pick predictor bands from: ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12']
 
 #Image to classify
-ImageName = 'Po_9_2018_S2' #do not add the tif extension
-ImageLocation = 'E:\\UAV2SEN\\FinalTif\\'#add slashes at the end
+ImageName = 'Paglia2018_SR' #do not add the tif extension
+ImageLocation = 'E:\\UAV2SEN\\SuperRes\\'#add slashes at the end
 ImageUTM = 32 #Give UTM zone. Assumes WGS84 datum
 North = True #set to false if wanting UTM for southern hemisphere
 SizeThresh = 500000 #This sets the threshold to trigger slower but OoM-error free row-by-row processing.  Increase or Decrease depending on your computer.
